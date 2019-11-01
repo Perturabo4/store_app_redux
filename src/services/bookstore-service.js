87ab9238944ee60086671766ec703c13eeb971b1
Perturabo4 <perturabo40@gmail.com>
2019-11-01 16:15:11 +0200

    Basic structure of project todo with redux

---
+++ src/services/bookstore-service.js
@@ -0,0 +1,8 @@
+export default class BookstoreService {
+    getBooks() {
+        return [
+            {id: 1, name: 'Dark disciple'},
+            {id: 2, name: 'Horus heresy'}
+        ];
+    }
+}
